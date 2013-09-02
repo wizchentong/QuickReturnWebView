63298a2f4482c508ae761471bd265d9482fa2a64
darktiny <dt2dev@gmail.com>
2013-09-02 14:18:46 +0800

    fix titlebar disappeared when fast scroll to top

--- src/me/dt2dev/quickreturnwebview/QuickReturnWebView.java
+++ src/me/dt2dev/quickreturnwebview/QuickReturnWebView.java
@@ -1,32 +1,46 @@
 package me.dt2dev.quickreturnwebview;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.TranslateAnimation;
 
 import com.nineoldandroids.view.ViewHelper;
+import com.nineoldandroids.view.ViewPropertyAnimator;
 import com.nobu_games.android.view.web.TitleBarWebView;
 
 public class QuickReturnWebView extends TitleBarWebView {
 
-	private int mLastMotionY;
 	private int mTitleBarHeight;
+	private int mMinTranslationY;
+	private int mMaxTranslationY;
 
 	private View mTitleBar;
 
+	private BounceHandler mBounceHandler;
+
 	public QuickReturnWebView(Context context) {
 		super(context);
+		init();
 	}
 
 	public QuickReturnWebView(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		init();
 	}
 
 	public QuickReturnWebView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
+		init();
+	}
+
+	private void init() {
+		mTitleBarHeight = 0;
+		mMaxTranslationY = 0;
+		mBounceHandler = new BounceHandler();
 	}
 
 	public void setTitleBar(View titleBar) {
@@ -36,6 +50,7 @@ public class QuickReturnWebView extends TitleBarWebView {
 			@Override
 			public void run() {
 				mTitleBarHeight = mTitleBar.getHeight();
+				mMinTranslationY = -1 * mTitleBarHeight;
 			}
 		});
 	}
@@ -50,65 +65,77 @@ public class QuickReturnWebView extends TitleBarWebView {
 
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
-		final int action = event.getAction();
-		switch (action) {
+		switch (event.getActionMasked()) {
 		case MotionEvent.ACTION_DOWN:
-			mLastMotionY = getScrollY();
-			break;
-		case MotionEvent.ACTION_MOVE:
-			if (mTitleBar != null)
-				onActionMove(mLastMotionY, getScrollY());
-			mLastMotionY = getScrollY();
+			mBounceHandler.setEnable(false);
 			break;
 		case MotionEvent.ACTION_UP:
-			if (mTitleBar != null)
-				onActionUp(getScrollY());
+		case MotionEvent.ACTION_CANCEL:
+			mBounceHandler.setEnable(true);
+			mBounceHandler.sendEmptyMessage(0);
 			break;
 		}
 		return super.onTouchEvent(event);
 	}
 
-	public void onActionMove(int oldScrollY, int newScrollY) {
-		final int offset = newScrollY - oldScrollY;
-		if (newScrollY <= mTitleBarHeight && offset > 0) {
-			ViewHelper.setTranslationY(mTitleBar, -newScrollY);
+	/**
+	 * offset > 0: Page Down; offset < 0: Page Up
+	 */
+	@Override
+	protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+		if (null == mTitleBar)
+			return;
+		mBounceHandler.sendEmptyMessage(0);
+		final int offset = t - oldt;
+		if (t <= mTitleBarHeight && offset > 0) {
+			if (t > 0)
+				ViewHelper.setTranslationY(mTitleBar, -t);
 			return;
 		}
-		final int oldTranslationY = (int) ViewHelper.getTranslationY(mTitleBar);
+		final int oldTY = (int) ViewHelper.getTranslationY(mTitleBar);
+		int newTY = oldTY - offset;
 		if (offset > 0) {
-			if (oldTranslationY == -mTitleBarHeight)
+			if (oldTY == mMinTranslationY)
 				return;
-			ViewHelper.setTranslationY(mTitleBar,
-					-Math.min(offset - oldTranslationY, mTitleBarHeight));
-		} else {
-			if (oldTranslationY == 0)
+			newTY = Math.max(newTY, mMinTranslationY);
+			ViewHelper.setTranslationY(mTitleBar, newTY);
+		} else if (offset < 0) {
+			if (oldTY == mMaxTranslationY)
 				return;
-			ViewHelper.setTranslationY(mTitleBar,
-					Math.min(oldTranslationY - offset, 0));
+			newTY = Math.min(newTY, mMaxTranslationY);
+			ViewHelper.setTranslationY(mTitleBar, newTY);
 		}
 	}
 
-	public void onActionUp(int scrollY) {
-		Animation animation;
-		final int translationY = (int) ViewHelper.getTranslationY(mTitleBar);
-		if (-translationY * 2 < mTitleBarHeight) {
-			ViewHelper.setTranslationY(mTitleBar, 0);
-			animation = new TranslateAnimation(Animation.ABSOLUTE, 0,
-					Animation.ABSOLUTE, 0, Animation.ABSOLUTE, translationY,
-					Animation.ABSOLUTE, 0);
-			animation.setDuration(300);
-			animation.setFillAfter(false);
-			mTitleBar.startAnimation(animation);
-		} else {
-			if (scrollY < mTitleBarHeight)
-				scrollTo(0, mTitleBarHeight);
-			animation = new TranslateAnimation(Animation.ABSOLUTE, 0,
-					Animation.ABSOLUTE, 0, Animation.ABSOLUTE, translationY,
-					Animation.ABSOLUTE, -mTitleBarHeight);
-			animation.setDuration(300);
-			animation.setFillAfter(false);
-			mTitleBar.startAnimation(animation);
-			ViewHelper.setTranslationY(mTitleBar, -mTitleBarHeight);
+	@SuppressLint("HandlerLeak")
+	private class BounceHandler extends Handler {
+
+		private static final int ANIMATION_DURATION = 300;
+
+		private boolean enable;
+
+		public void setEnable(boolean enable) {
+			this.enable = enable;
+		}
+
+		@Override
+		public void handleMessage(Message msg) {
+			if (!enable || null == mTitleBar || getScrollY() < mTitleBarHeight)
+				return;
+			final int oldTY = (int) ViewHelper.getTranslationY(mTitleBar);
+			if (oldTY == mMinTranslationY || oldTY == mMaxTranslationY)
+				return;
+			if (oldTY * 2 > mMinTranslationY)
+				startAnimation(mTitleBar, mMaxTranslationY);
+			else
+				startAnimation(mTitleBar, mMinTranslationY);
+		}
+
+		private void startAnimation(View view, int translationY) {
+			ViewPropertyAnimator.animate(view).cancel();
+			ViewPropertyAnimator.animate(view).setDuration(ANIMATION_DURATION)
+					.translationY(translationY);
 		}
 	}
+
 }
