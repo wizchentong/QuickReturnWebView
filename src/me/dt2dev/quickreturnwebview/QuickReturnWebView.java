d39c86646d53b229e2d28c37fa85026d22355235
darktiny <dt2dev@gmail.com>
2013-09-05 17:53:57 +0800

    大幅减少Padding区域出现的机率

--- src/me/dt2dev/quickreturnwebview/QuickReturnWebView.java
+++ src/me/dt2dev/quickreturnwebview/QuickReturnWebView.java
@@ -1,9 +1,6 @@
 package me.dt2dev.quickreturnwebview;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.os.Handler;
-import android.os.Message;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -18,9 +15,9 @@ public class QuickReturnWebView extends TitleBarWebView {
 	private int mMinTranslationY;
 	private int mMaxTranslationY;
 
-	private View mTitleBar;
+	private boolean isActionUpOrCancel = false;
 
-	private BounceHandler mBounceHandler;
+	private View mTitleBar;
 
 	public QuickReturnWebView(Context context) {
 		super(context);
@@ -40,7 +37,6 @@ public class QuickReturnWebView extends TitleBarWebView {
 	private void init() {
 		mTitleBarHeight = 0;
 		mMaxTranslationY = 0;
-		mBounceHandler = new BounceHandler();
 	}
 
 	public void setTitleBar(View titleBar) {
@@ -67,15 +63,37 @@ public class QuickReturnWebView extends TitleBarWebView {
 	public boolean onTouchEvent(MotionEvent event) {
 		switch (event.getActionMasked()) {
 		case MotionEvent.ACTION_DOWN:
-			mBounceHandler.setEnable(false);
+			isActionUpOrCancel = false;
 			break;
 		case MotionEvent.ACTION_UP:
 		case MotionEvent.ACTION_CANCEL:
-			mBounceHandler.setEnable(true);
-			mBounceHandler.sendEmptyMessage(0);
+			isActionUpOrCancel = true;
+			onActionUpOrCancel();
 			break;
 		}
 		return super.onTouchEvent(event);
+	}
+
+	private void onActionUpOrCancel() {
+		if (null == mTitleBar)
+			return;
+		final int oldTY = (int) ViewHelper.getTranslationY(mTitleBar);
+		if (oldTY == mMinTranslationY || oldTY == mMaxTranslationY)
+			return;
+		final int scrollY = getScrollY();
+		if (scrollY < 0) {
+			ViewHelper.setTranslationY(mTitleBar, mMaxTranslationY);
+			return;
+		}
+		if (oldTY * 2 > mMinTranslationY || scrollY < mTitleBarHeight)
+			startAnimation(mTitleBar, mMaxTranslationY);
+		else
+			startAnimation(mTitleBar, mMinTranslationY);
+	}
+
+	private void startAnimation(View view, int translationY) {
+		ViewPropertyAnimator.animate(view).cancel();
+		ViewPropertyAnimator.animate(view).translationY(translationY);
 	}
 
 	/**
@@ -85,7 +103,9 @@ public class QuickReturnWebView extends TitleBarWebView {
 	protected void onScrollChanged(int l, int t, int oldl, int oldt) {
 		if (null == mTitleBar)
 			return;
-		mBounceHandler.sendEmptyMessage(0);
+		ViewPropertyAnimator.animate(mTitleBar).cancel();
+		if (isActionUpOrCancel)
+			onActionUpOrCancel();
 		final int offset = t - oldt;
 		if (t <= mTitleBarHeight && offset > 0) {
 			if (t > 0)
@@ -104,37 +124,6 @@ public class QuickReturnWebView extends TitleBarWebView {
 				return;
 			newTY = Math.min(newTY, mMaxTranslationY);
 			ViewHelper.setTranslationY(mTitleBar, newTY);
-		}
-	}
-
-	@SuppressLint("HandlerLeak")
-	private class BounceHandler extends Handler {
-
-		private static final int ANIMATION_DURATION = 300;
-
-		private boolean enable;
-
-		public void setEnable(boolean enable) {
-			this.enable = enable;
-		}
-
-		@Override
-		public void handleMessage(Message msg) {
-			if (!enable || null == mTitleBar || getScrollY() < mTitleBarHeight)
-				return;
-			final int oldTY = (int) ViewHelper.getTranslationY(mTitleBar);
-			if (oldTY == mMinTranslationY || oldTY == mMaxTranslationY)
-				return;
-			if (oldTY * 2 > mMinTranslationY)
-				startAnimation(mTitleBar, mMaxTranslationY);
-			else
-				startAnimation(mTitleBar, mMinTranslationY);
-		}
-
-		private void startAnimation(View view, int translationY) {
-			ViewPropertyAnimator.animate(view).cancel();
-			ViewPropertyAnimator.animate(view).setDuration(ANIMATION_DURATION)
-					.translationY(translationY);
 		}
 	}
 
